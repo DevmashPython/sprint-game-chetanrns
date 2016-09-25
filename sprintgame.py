d5c761883837bb05e93ae0f6783f7676ba110679
chetanrns <chetanbanavikalmutt1997@gmail.com>
2016-09-25 20:30:31 +0530

    firstcommit

---
+++ sprintgame.py
@@ -0,0 +1,74 @@
+import time
+import msvcrt
+finish=10
+count=0
+print "Press 'D' to move right"
+print "Press 'W' to move down"
+
+print "press enter key to get started!"
+
+raw_input()
+s_time=time.time()
+def move_right():
+	count=0
+	while(1):
+		key=msvcrt.getch()
+		if key=='d':
+			count=count+1
+			print "d",
+			if count==finish:
+				print "Move down"
+				move_down()
+				break
+		else:
+			print "GAME OVER"
+			break
+			
+				
+	return
+def move_down():
+	count=0
+	while(1):
+		key=msvcrt.getch()
+		if key=='s':
+			count=count+1
+			print "\t\t\ts"
+			if count==finish:
+				print "\t\t\tMove right",
+				break
+		else:
+			print "GAME OVER"
+			break
+				
+	return
+	
+move_right()
+count=0
+while(1):
+	key=msvcrt.getch()
+	if key=='d':
+		count=count+1
+		print "d",
+		if count==finish:
+			print "\nyou've successfully completed the game"
+			break
+	else:
+		print "GAME OVER"
+		break
+f_time=time.time()-s_time
+
+print "time taken:"+str(f_time)
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
